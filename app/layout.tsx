2abd96fbfc077295da734d607c0666a647ff5719
Harsh Sharma <harsh.sharma.001@juspay.in>
2025-07-09 09:06:40 +0530

    enable insights and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Funnel_Display } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} ${funnelDisplay.variable} antialiased`}
       >
         <Toaster />
+        <Analytics />
         {children}
       </body>
     </html>
